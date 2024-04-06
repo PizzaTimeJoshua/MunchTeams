0ef0ca6aff76a0a6584adc6bbcf0f4d3b2867d1f
PizzaTimeJoshua <114630401+PizzaTimeJoshua@users.noreply.github.com>
2024-04-05 20:00:53 -0700

    No Date + Reg G

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, jsonify, request
 import os
 import pyjson5
-from datetime import datetime
 import difflib
 import re
 import pandas as pd
 
 FORMATS = [
+    "gen9vgc2024regg",
+    "gen9vgc2024reggbo3",
     "gen9vgc2024regf",
     "gen9vgc2024regfbo3",
     "gen9nationaldex",
@@ -35,7 +36,7 @@ def safe_load_files():
             pokedexRaw = file.read()
         pokedexData = pyjson5.loads(pokedexRaw)
 
-def find_replays(pokeSearch,meta,replay_total=100,filters={ "teamused":False, "rating" : 0, "winner": False,"min_upload": 0, "max_upload" : 0, "allow_duplicate_players" : False}):
+def find_replays(pokeSearch,meta,replay_total=100,filters={ "teamused":False, "rating" : 0, "winner": False, "allow_duplicate_players" : False}):
     global thread_status
     with open(f"data/search-replays-list-{meta}.json","rb") as file:
         replay_data = pyjson5.load(file)
@@ -60,11 +61,6 @@ def find_replays(pokeSearch,meta,replay_total=100,filters={ "teamused":False, "r
     # Winner Filter
     if (filters["winner"] and (pokeSearch != [])):
         search_filter.append((df['teams'].apply(lambda x: set(pokeSearch).issubset(x[0])) & (df['winner_index'] == 1)) | (df['teams'].apply(lambda x: set(pokeSearch).issubset(x[1])) & (df['winner_index'] == 2))  )
-    # Day Filter
-    if (filters["max_upload"]>0):
-        search_filter.append((df['uploadtime'] >= filters["min_upload"]) & (df['uploadtime'] <= filters["max_upload"]))
-    else:
-        search_filter.append(df['uploadtime'] >= filters["min_upload"])
     # Duplicate Players Filter
     if not (filters.get("allow_duplicate_players",False)):
         search_filter.append((~df.duplicated(subset=['winner'], keep='first')))
@@ -109,19 +105,12 @@ def load_replay():
     filter_battleused = request.args.get('filter_battleused') == 'true'
     filter_rating_enabled = request.args.get('filter_rating_enabled') == 'true'
     filter_winner = request.args.get('filter_winner') == 'true'
-    filter_date_enabled = request.args.get('filter_date_enabled') == 'true'
 
     pokemon_search = request.args.get('pokemon_search')
     filter_format = request.args.get('filter_format')
     filter_rating = request.args.get('filter_rating')
-    filter_day_min = request.args.get('filter_day_min')
-    filter_day_max = request.args.get('filter_day_max')
 
-    if (filter_date_enabled == False):
-        filter_day_min = 0
-        filter_day_max = 0
-
-    filters={ "teamused":filter_battleused, "rating" : 0, "winner": filter_winner,"min_upload": int(filter_day_min), "max_upload" : int(filter_day_max) }
+    filters={ "teamused":filter_battleused, "rating" : 0, "winner": filter_winner}
 
     
     if (filter_rating_enabled and filter_rating.isnumeric()):
