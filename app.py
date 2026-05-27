46905d707fd54a553fd392c5a5b89b093c4cbee6
PizzaTimeJoshua <joshua.diaz244@gmail.com>
2026-05-27 01:23:50 -0700

    Commit changes via Python Script

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import re
 FORMATS = [
     "gen9championsvgc2026regmabo3",
     "gen9championsvgc2026regma",
+    "gen9championsou",
+    "gen9championsbssregma",
     "gen9vgc2026regibo3",
     "gen9vgc2026regi",
     "gen9nationaldex",
